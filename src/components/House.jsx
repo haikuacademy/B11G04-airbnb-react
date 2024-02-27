b62fb59380808e749c1e06c49740f6988304ce2d
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-27 17:04:10 +0700

    coplmete house layout

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -15,27 +15,29 @@ function House() {
           <div className="bg-pink-600">9</div>
         </div>
       </div>
-      {/* <div className="grid grid-cols-2">
-        <div className="col-span-2 gap-5 bg-pink-200">
+      <div className="grid grid-cols-3 gap-36 bg-pink-200">
+        <div className="col-span-2 bg-pink-200">
           <div className="bg-pink-600">3</div>
-          <div className="flex bg-blue-200">
+          <div className="flex gap-1 bg-blue-200">
             <div className="bg-blue-600">3.1</div>
             <div className="bg-blue-600">3.2</div>
           </div>
-        </div>
-        <div className="bg-pink-200">
-          <div className="bg-pink-600">4</div>
-          <div className="flex bg-blue-200">
+          <div className="flex gap-1 bg-blue-200">
             <div className="bg-blue-600">5.1</div>
             <div className="bg-blue-600">5.2</div>
           </div>
         </div>
+        <div className="bg-pink-200">
+          <div className="bg-pink-200">
+            <div className="bg-pink-600">4</div>
+          </div>
+          <div className="flex justify-between bg-blue-200">
+            <div className="bg-blue-600">4.1</div>
+            <div className="bg-blue-600">4.2</div>
+          </div>
+          <div className="bg-pink-600">6</div>
+        </div>
       </div>
-      <div className="flex bg-blue-200">
-        <div className="bg-blue-600">4.1</div>
-        <div className="bg-blue-600">4.2</div>
-      </div>
-      <div className="bg-pink-600">6</div> */}
     </div>
   )
 }

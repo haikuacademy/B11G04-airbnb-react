455bb795169c926e94c72dbcf94b156cb48f624f
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-27 12:42:34 +0700

    fix conflict

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Houses from './components/Houses'
 import House from './components/House'
 import HouseEdit from './components/HouseEdit'
-
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Bookings from './components/Bookings'
-
 import Profile from './components/Profile'
 
 function Router() {
@@ -19,6 +17,7 @@ function Router() {
                 <Route path='/houses/:id/edit' element={<HouseEdit />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/signup' element={<Signup />} />
+                <Route path='/bookings' element={<Bookings />} />
                 <Route path='/profile' element={<Profile />} />
             </Routes>
         </BrowserRouter>

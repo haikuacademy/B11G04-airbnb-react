467e5a27b7bf0edae015a76bc0c317b8237a34e0
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-27 15:06:03 +0700

    comment second section of grid

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,33 +1,41 @@
 function House() {
   return (
     <div className="container mx-auto">
-      <div className="flex justify-between bg-blue-200">
-        <div className="bg-blue-600">1</div>
-        <div className="bg-blue-600">2</div>
-        <div className="bg-blue-600">3</div>
-        <div className="bg-blue-600">4</div>
-        <div className="bg-blue-600">5</div>
-        <div className="bg-blue-600">6</div>
-      </div>
-      <div className="grid">
-        <div className="cols-span-2">
+      <div className="grid grid-cols-2 gap-5 bg-pink-200">
+        <div className="bg-pink-600">1</div>
+        <div className="grid grid-cols-3 gap-2 bg-pink-200">
+          <div className="bg-pink-600">1</div>
+          <div className="bg-pink-600">2</div>
           <div className="bg-pink-600">3</div>
-          <div className="flex">
+          <div className="bg-pink-600">4</div>
+          <div className="bg-pink-600">5</div>
+          <div className="bg-pink-600">6</div>
+          <div className="bg-pink-600">7</div>
+          <div className="bg-pink-600">8</div>
+          <div className="bg-pink-600">9</div>
+        </div>
+      </div>
+      {/* <div className="grid grid-cols-2">
+        <div className="col-span-2 gap-5 bg-pink-200">
+          <div className="bg-pink-600">3</div>
+          <div className="flex bg-blue-200">
             <div className="bg-blue-600">3.1</div>
             <div className="bg-blue-600">3.2</div>
           </div>
-          <div className="flex">
-            <bg-blue-600>5.1</bg-blue-600>
-            <bg-blue-600>5.2</bg-blue-600>
+        </div>
+        <div className="bg-pink-200">
+          <div className="bg-pink-600">4</div>
+          <div className="flex bg-blue-200">
+            <div className="bg-blue-600">5.1</div>
+            <div className="bg-blue-600">5.2</div>
           </div>
         </div>
-        <div className="bg-pink-600">4</div>
-        <div className="flex">
-          <bg-blue-600>4.1</bg-blue-600>
-          <bg-blue-600>4.2</bg-blue-600>
-        </div>
-        <div className="bg-pink-600">6</div>
       </div>
+      <div className="flex bg-blue-200">
+        <div className="bg-blue-600">4.1</div>
+        <div className="bg-blue-600">4.2</div>
+      </div>
+      <div className="bg-pink-600">6</div> */}
     </div>
   )
 }

a0dbeda958fdc8737bb2d238ae49e6ddcff1d1cf
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-27 14:21:01 +0700

    add emmett layout

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,8 +1,40 @@
 function Houses() {
   return (
-    <div>
-      <p>Hello from Houses!</p>
+ <div className="container">
+  <div className="grid">
+    <div className="bg-pink-600">1</div>
+    <div className="grid cols-3">
+      <div className="bg-pink-600">1</div>
+      <div className="bg-pink-600">2</div>
+      <div className="bg-pink-600">3</div>
+      <div className="bg-pink-600">4</div>
+      <div className="bg-pink-600">5</div>
+      <div className="bg-pink-600">6</div>
+      <div className="bg-pink-600">7</div>
+      <div className="bg-pink-600">8</div>
+      <div className="bg-pink-600">9</div>
     </div>
+  </div>
+ <div className="grid">
+    <div className="cols-span-2">    
+    <div className="bg-pink-600"></div>
+    <div className="flex">
+      <div className="bg-blue-600"></div>
+      <div className="bg-blue-600"></div>
+    </div>
+    <div className="flex">
+      <bg-blue-600></bg-blue-600>
+      <bg-blue-600></bg-blue-600>
+    </div>
+  </div>
+  <div className="bg-pink-600"></div>
+  <div className="flex">
+    <bg-blue-600></bg-blue-600>
+    <bg-blue-600></bg-blue-600>
+  </div>
+  <div className="bg-pink-600"></div>
+ </div>
+ </div>
   )
 }
 

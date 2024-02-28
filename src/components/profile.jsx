327af3e38e700ab11c6b55fc727eb3c1ffad21bc
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-28 11:51:31 +0700

    Imported Nav component into remaining pages

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,9 @@
+import Nav from './Nav'
+
 function Profile() {
   return (
     <div className="container mx-auto">
+      <Nav />
       <div className="flex gap-2 justify-start bg-blue-200">
         <div className="bg-blue-600">1</div>
         <div className="bg-blue-600">2</div>

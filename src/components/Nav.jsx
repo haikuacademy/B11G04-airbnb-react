57230d90bbbd3450eb32ebc590d53e13403f81e9
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-29 12:52:01 +0700

    Configured styling for nav bar elements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,22 @@
 function Nav() {
   return (
-    <div>
-      <p>Hello from Nav!</p>
+    <div className="flex justify-between">
+      <div>
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png"
+          alt="Airbnb logo"
+          className="h-7 mt-3 mb-1"
+        />
+      </div>
+      <div className="flex mt-3 mb-1 gap-1">
+        <div className="inline border rounded text-sm px-2 py-1">
+          My Bookings
+        </div>
+        <div className="inline border rounded text-sm px-2 py-1">
+          My Listings
+        </div>
+        <div className="inline border rounded text-sm px-2 py-1">Profile</div>
+      </div>
     </div>
   )
 }

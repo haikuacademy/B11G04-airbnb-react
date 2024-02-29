0f3ae6564f2299b10b8c72f377c8ddbb67ef1847
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-29 11:32:53 +0700

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,11 @@
 function Signup() {
   return (
-    <div>
+    <div className="container mx-auto">
+      <img
+        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png
+"
+        alt=""
+      />
       <p>Hello from Signup!</p>
     </div>
   )

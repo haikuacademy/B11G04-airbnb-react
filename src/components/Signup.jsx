818d5d3a697e95f93dc5151c2b59ab7668c932e7
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-29 12:13:04 +0700

    create all elements

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,13 +1,29 @@
+import { Link } from 'react-router-dom'
+
 function Signup() {
   return (
-    <div className="container mx-auto">
+    <form className="container mx-auto flex flex-col p-8 border-2 rounded">
       <img
         src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png
 "
-        alt=""
+        alt="airbnb logo"
+        className="w-32"
       />
-      <p>Hello from Signup!</p>
-    </div>
+      <label>First Name</label>
+      <input type="text" className="border-2 p-1" />
+      <label>Last Name</label>
+      <input type="text" className="border-2 p-1" />
+      <label>Email</label>
+      <input type="email" className="border-2 p-1" />
+      <label>Password</label>
+      <input type="password" className="border-2 p-1" />
+      <label>Profile Picture</label>
+      <input type="url" className="border-2 p-1" />
+      <button className="bg-pink-500 text-white bg-[#FB7185]">Register</button>
+      <span>
+        Already have an account? <Link to="/login">Login here</Link>
+      </span>
+    </form>
   )
 }
 

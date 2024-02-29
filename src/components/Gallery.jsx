89dc0810d19a608eee282282d5b059a2461a67a2
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-29 11:00:45 +0700

    Rounded corners of images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,43 +4,53 @@ function Gallery() {
       <img
         src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
         alt="House 1 image 1"
+        className="rounded"
       />
       <div className="grid grid-cols-3 gap-2">
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
           alt="House 1 image 1"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_02.png"
           alt="House 1 image 2"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png"
           alt="House 1 image 3"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_04.png"
           alt="House 1 image 4"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_05.png"
           alt="House 1 image 5"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_06.png"
           alt="House 1 image 6"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_07.png"
           alt="House 1 image 7"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_08.png"
           alt="House 1 image 8"
+          className="rounded"
         />
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_09.png"
           alt="House 1 image 9"
+          className="rounded"
         />
       </div>
     </div>

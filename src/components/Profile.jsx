40ed42df27d0e9098331eab9bb8da020b255fcba
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-29 14:21:36 +0700

    Finish Profile Form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,12 +2,44 @@ import Nav from './Nav'
 
 function Profile() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto max-w-[1535px]">
       <Nav />
-      <div className="flex gap-2 justify-start ">
-        <div>1</div>
-        <div>2</div>
-      </div>
+      <form className="flex flex-col gap-2 justify-start border-2 rounded p-5 mt-4">
+        <h1 className="font-bold text-2xl">Your Profile</h1>
+        <div className="flex">
+          <img
+            src="https://www.generatormix.com/images/muppets/pepe-the-king-prawn.jpg"
+            alt="pepe the king prawn"
+            className="w-20 rounded-full p-2"
+          />
+          <input
+            className="border-2 px-4 py-2 p-1 rounded w-full"
+            type="text"
+            placeholder="https://www.generatormix.com/images/muppets/pepe-the-king-prawn.jpg"
+          />
+        </div>
+        <label>First Name</label>
+        <input
+          className="border-2 px-4 py-2 p-1 rounded"
+          type="text"
+          placeholder="Pepe"
+        />
+        <label>Last Name</label>
+        <input
+          className="border-2 px-4 py-2 p-1 rounded"
+          type="text"
+          placeholder="the king prawn"
+        />
+        <label>Email</label>
+        <input
+          className="border-2 px-4 py-2 p-1 rounded"
+          type="email"
+          placeholder="pepe@prawnholdings.com"
+        />
+        <button className="rounded p-3 mt-6 bg-pink-500 text-white w-32 bg-[#FB7185]">
+          Save Changes
+        </button>
+      </form>
     </div>
   )
 }

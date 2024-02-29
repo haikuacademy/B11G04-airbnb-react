51f8428401744712dde2e6b3750b17c925fcb6d1
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-29 10:44:35 +0700

    Update HouseCard.jsx

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,6 +1,12 @@
 function HouseCard() {
   return (
-    <div className="p-2 border rounded">
+    <div className=" border rounded">
+      <img
+        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png
+"
+        className="border rounded"
+        alt=""
+      />
       <h6 className="text-lg font-bold">Phuket, Thailand</h6>
       <span className="text-sm text-slate-400">2 rooms · 2 bathrooms</span>
       <h5 className="text-lg font-bold">$120</h5>

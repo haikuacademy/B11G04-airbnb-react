48019c97bc1fc45087f9731076a08b8681b91769
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-29 13:24:16 +0700

    Update HouseCard.jsx

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -14,27 +14,31 @@ function HouseCard() {
         <img
           src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png
 "
-        className="border rounded"
-        alt=""
-      />
-      <h6 className="text-lg font-bold">Phuket, Thailand</h6>
-      <span className="text-sm text-slate-400">2 rooms · 2 bathrooms</span>
-      <h5 className="text-lg font-bold">$120</h5>
-      <div className="flex justify-between">
-        <div>
-          <span>
-            <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
-            <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
-            <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
-            <FontAwesomeIcon className="text-yellow-500" icon={faStarHalf} />
-          </span>
-          4.5
+          className="border rounded"
+          alt=""
+        />
+        <h6 className="text-lg font-bold">Phuket, Thailand</h6>
+        <span className="text-sm text-slate-400">2 rooms · 2 bathrooms</span>
+        <h5 className="text-lg font-bold">$120</h5>
+        <div className="flex justify-between">
+          <div>
+            <span>
+              <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
+              <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
+              <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
+              <FontAwesomeIcon className="text-yellow-500" icon={faStarHalf} />
+            </span>
+            4.5
+          </div>
+          <div>
+            34{' '}
+            <FontAwesomeIcon
+              className="text-transoarent"
+              icon={faCommentDots}
+            />
+          </div>
         </div>
-        <div>
-          34{' '}
-          <FontAwesomeIcon className="text-transoarent" icon={faCommentDots} />
-        </div>
-      </div>
+      </Link>
     </div>
   )
 }

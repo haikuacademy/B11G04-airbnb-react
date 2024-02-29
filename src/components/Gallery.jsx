a95cbcf40fe5361ed1aa3bee96c5df74a670de60
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-29 10:55:33 +0700

    Added gallery images to placeholder divs

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,47 @@
 function Gallery() {
   return (
     <div className="grid grid-cols-2 gap-5 bg-pink-200">
-      <div className="bg-pink-600">1</div>
+      <img
+        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
+        alt="House 1 image 1"
+      />
       <div className="grid grid-cols-3 gap-2 bg-pink-200">
-        <div className="bg-pink-600">1</div>
-        <div className="bg-pink-600">2</div>
-        <div className="bg-pink-600">3</div>
-        <div className="bg-pink-600">4</div>
-        <div className="bg-pink-600">5</div>
-        <div className="bg-pink-600">6</div>
-        <div className="bg-pink-600">7</div>
-        <div className="bg-pink-600">8</div>
-        <div className="bg-pink-600">9</div>
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png"
+          alt="House 1 image 1"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_02.png"
+          alt="House 1 image 2"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_03.png"
+          alt="House 1 image 3"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_04.png"
+          alt="House 1 image 4"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_05.png"
+          alt="House 1 image 5"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_06.png"
+          alt="House 1 image 6"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_07.png"
+          alt="House 1 image 7"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_08.png"
+          alt="House 1 image 8"
+        />
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_09.png"
+          alt="House 1 image 9"
+        />
       </div>
     </div>
   )

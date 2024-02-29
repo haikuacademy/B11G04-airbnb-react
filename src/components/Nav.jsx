9dfc375ab51dc32d496100742f1d4ece7f7a9d78
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-29 12:57:30 +0700

    Set up links to pages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,33 @@
+import { Link } from 'react-router-dom'
+
 function Nav() {
   return (
     <div className="flex justify-between">
       <div>
-        <img
-          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png"
-          alt="Airbnb logo"
-          className="h-7 mt-3 mb-1"
-        />
+        <Link to="/">
+          <img
+            src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png"
+            alt="Airbnb logo"
+            className="h-7 mt-3 mb-1"
+          />
+        </Link>
       </div>
       <div className="flex mt-3 mb-1 gap-1">
-        <div className="inline border rounded text-sm px-2 py-1">
+        <Link
+          to="/bookings"
+          className="inline border rounded text-sm px-2 py-1"
+        >
           My Bookings
-        </div>
-        <div className="inline border rounded text-sm px-2 py-1">
+        </Link>
+        <Link
+          to="/listings"
+          className="inline border rounded text-sm px-2 py-1"
+        >
           My Listings
-        </div>
-        <div className="inline border rounded text-sm px-2 py-1">Profile</div>
+        </Link>
+        <Link to="/profile" className="inline border rounded text-sm px-2 py-1">
+          Profile
+        </Link>
       </div>
     </div>
   )

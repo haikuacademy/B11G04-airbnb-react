d0cc9e13cbf91f022d3f27513762750c5db804a9
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-29 22:46:41 +0700

    login first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,40 @@
 function Login() {
   return (
-    <div>
-      <p>Hello from Login!</p>
+    <div className="flex mx-auto justify-center m-20 rounded-lg">
+      <div className="border rounder p-4">
+        <div className="flex items-center justify-center text-red-500">
+          <strong>airbnb</strong>
+        </div>
+        <div className="m-3 pb-3">
+          <div>Email</div>
+          <div>
+            <form>
+              <input type="email" className="border" />
+            </form>
+          </div>
+        </div>
+        <div className="m-3 pb-3">
+          <div>
+            <div>Password</div>
+            <form>
+              <input type="password" className="border" />
+            </form>
+          </div>
+        </div>
+        <div className="m-3 pb-3">
+          <div className="flex items-center justify-center text-white bg bg-red-400 ">
+            <form>
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
+        <div className="flex justify-start m-3 pb-3 text-xs">
+          <div>New to Airbnb?</div>
+          <div className="text-red-500 ; underline md:underline-offset-2 ">
+            Create an Account
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

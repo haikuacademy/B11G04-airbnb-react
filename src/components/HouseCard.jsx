643b3bdb10be9c6447e0f84e7b329766467364b9
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-29 12:56:19 +0700

    Update HouseCard.jsx

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,19 +1,23 @@
+import { Link } from 'react-router-dom'
+
 function HouseCard() {
   return (
-    <div className=" border rounded">
-      <img
-        src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png
+    <div className=" border rounded hover:shadow">
+      <Link to="/houses/1">
+        <img
+          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png
 "
-        className="border rounded"
-        alt=""
-      />
-      <h6 className="text-lg font-bold">Phuket, Thailand</h6>
-      <span className="text-sm text-slate-400">2 rooms · 2 bathrooms</span>
-      <h5 className="text-lg font-bold">$120</h5>
-      <div className="flex justify-between">
-        <div>4.5</div>
-        <div>34</div>
-      </div>
+          className="border rounded"
+          alt=""
+        />
+        <h6 className="text-lg font-bold">Phuket, Thailand</h6>
+        <span className="text-sm text-slate-400">2 rooms · 2 bathrooms</span>
+        <h5 className="text-lg font-bold">$120</h5>
+        <div className="flex justify-between">
+          <div>4.5</div>
+          <div>34</div>
+        </div>
+      </Link>
     </div>
   )
 }

737cf0f6ccaa9022bf134be0e5a93ca7d03e0a33
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-03-01 11:40:01 +0700

    resolved suggestions

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,41 +2,41 @@ import Nav from './Nav'
 
 function Profile() {
   return (
-    <div className="container mx-auto max-w-[1535px]">
+    <div className="container mx-auto ">
       <Nav />
       <form className="flex flex-col gap-2 justify-start border-2 rounded p-5 mt-4">
         <h1 className="font-bold text-2xl">Your Profile</h1>
-        <div className="flex">
+        <div className="flex items-center">
           <img
             src="https://www.generatormix.com/images/muppets/pepe-the-king-prawn.jpg"
             alt="pepe the king prawn"
-            className="w-20 rounded-full p-2"
+            className="w-20 rounded-full"
           />
           <input
-            className="border-2 px-4 py-2 p-1 rounded w-full"
+            className="border-2 px-4 py-2     rounded w-full ml-4"
             type="text"
-            placeholder="https://www.generatormix.com/images/muppets/pepe-the-king-prawn.jpg"
+            value="https://www.generatormix.com/images/muppets/pepe-the-king-prawn.jpg"
           />
         </div>
         <label>First Name</label>
         <input
           className="border-2 px-4 py-2 p-1 rounded"
           type="text"
-          placeholder="Pepe"
+          value="Pepe"
         />
         <label>Last Name</label>
         <input
           className="border-2 px-4 py-2 p-1 rounded"
           type="text"
-          placeholder="the king prawn"
+          value="the king prawn"
         />
         <label>Email</label>
         <input
           className="border-2 px-4 py-2 p-1 rounded"
           type="email"
-          placeholder="pepe@prawnholdings.com"
+          value="pepe@prawnholdings.com"
         />
-        <button className="rounded p-3 mt-6 bg-pink-500 text-white w-32 bg-[#FB7185]">
+        <button className="rounded p-3 mt-6  text-white w-32 bg-red-400">
           Save Changes
         </button>
       </form>

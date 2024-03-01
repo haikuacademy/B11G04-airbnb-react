4d893a2e10821fdc90aea9a9d7637f772ee38eea
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-03-01 13:07:43 +0700

    Seperate CreateListing & HouseEdit

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -4,10 +4,52 @@ function HouseEdit() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <div className="grid grid-cols-2 gap-20 ">
-        <div>1</div>
-        <div>2</div>
-      </div>
+      <form className="p-4 mx-2 border-2 rounded">
+        <h1 className="my-1 text-2xl">List a house</h1>
+        <div className="grid grid-cols-2">
+          <div className=" mr-28">
+            <div className="flex flex-col my-1">
+              <label>Location</label>
+              <input
+                className="border-2 rounded p-2"
+                type="text"
+                value="Bali"
+              />
+            </div>
+            <div className="flex flex-col my-1">
+              <label>Bedrooms</label>
+              <input className="border-2 rounded p-2" type="text" />
+            </div>
+            <div className="flex flex-col my-1">
+              <label>Bathrooms</label>
+              <input className="border-2 rounded p-2" type="text" />
+            </div>
+            <div className="flex flex-col my-1">
+              <label>Price per Night</label>
+              <input className="border-2 rounded p-2" type="text" />
+            </div>
+            <div className="flex flex-col my-1">
+              <label>Description</label>
+              <textarea className="border-2 rounded p-2" rows="6"></textarea>
+            </div>
+            <button className="rounded p-3 mt-6 text-white w-32 bg-red-400">
+              List House
+            </button>
+          </div>
+          <div className="flex flex-col">
+            <div className="px-1">Photos</div>
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+            <input type="url" className="border-2 rounded p-2 my-1" />
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,35 +1,40 @@
 import Nav from './Nav'
 import HouseCard from './HouseCard'
 
-function HouseEdit() {
+function EditListing() {
   return (
     <form className="p-4 mx-2 border-2 rounded">
-      <h1 className="py-1 text-2xl">List a house</h1>
+      <h1 className="my-1 text-2xl">List a house</h1>
       <div className="grid grid-cols-2">
         <div className=" mr-28">
-          <div className="flex flex-col py-1">
+          <div className="flex flex-col my-1">
             <label>Location</label>
             <input className="border-2 rounded p-2" type="text" value="Bali" />
           </div>
-          <div className="flex flex-col py-1">
+          <div className="flex flex-col my-1">
             <label>Bedrooms</label>
             <input className="border-2 rounded p-2" type="text" />
           </div>
-          <div className="flex flex-col py-1">
+          <div className="flex flex-col my-1">
             <label>Bathrooms</label>
             <input className="border-2 rounded p-2" type="text" />
           </div>
-          <div className="flex flex-col py-1">
+          <div className="flex flex-col my-1">
             <label>Price per Night</label>
             <input className="border-2 rounded p-2" type="text" />
           </div>
-          <div className="flex flex-col py-1">
+          <div className="flex flex-col my-1">
             <label>Description</label>
             <textarea className="border-2 rounded p-2" rows="6"></textarea>
           </div>
-          <button className="rounded p-3 mt-6 text-white w-32 bg-red-400">
-            List House
-          </button>
+          <div className=" mt-6">
+            <button className="rounded  py-2  px-3 mr-2 text-white  bg-red-400">
+              List House
+            </button>
+            <button className="rounded  py-2  px-3 mt-6 text-black border-2 ">
+              Cancel
+            </button>
+          </div>
         </div>
         <div className="flex flex-col">
           <div className="px-1">Photos</div>
@@ -52,7 +57,7 @@ function Listings() {
   return (
     <div className="container mx-auto">
       <Nav />
-      <HouseEdit />
+      <EditListing />
       <div className="grid grid-cols-5 gap-4 mx-2">
         <HouseCard />
         <HouseCard />

7905b274724382003b3f3d6bb204b2e9421275a4
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-03-02 09:53:36 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,49 +2,51 @@ import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div className="flex mx-auto justify-center m-20 rounded-lg">
-      <div className="border rounder p-4">
-        <div className="flex items-center justify-center text-red-500">
-          <strong>airbnb</strong>
-        </div>
-        <div className="m-3 pb-3">
-          <div>Email</div>
-          <div>
-            <form>
+    <form>
+      <div className="flex mx-auto justify-center m-4 pb-3">
+        <div className="border p-4">
+          <div className="flex items-center justify-center">
+            <img
+              src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png
+"
+              alt="airbnb logo"
+              className="h-6 self-center"
+            />
+          </div>
+          <div className="m-3 pb-3">
+            <div>Email</div>
+            <div>
               <input type="email" className="border" />
-            </form>
+            </div>
           </div>
-        </div>
-        <div className="m-3 pb-3">
-          <div>
-            <div>Password</div>
-            <form>
+          <div className="m-3 pb-3">
+            <div>
+              <div>Password</div>
+
               <input type="password" className="border" />
-            </form>
+            </div>
           </div>
-        </div>
-        <div className="m-3 pb-3">
-          <div className="flex items-center justify-center text-white bg bg-red-400 ">
-            <form>
+          <div className="m-3 pb-3">
+            <div className="flex items-center justify-center text-white bg bg-red-400 rounded-md p-4">
               <button>Login</button>
-            </form>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-start m-3 pb-3 text-xs">
-          <div>New to Airbnb?</div>
-          <div>
-            <Link
-              to="/Signup"
-              className="text-red-500 ; text-xs ; underline
+          <div className="flex justify-start m-3 pb-3 text-xs">
+            <div>New to Airbnb?</div>
+            <div>
+              <Link
+                to="/signup"
+                className="text-red-500  text-xs  underline
               md:underline-offset-2 "
-            >
-              {' '}
-              Creat an Account
-            </Link>
+              >
+                {' '}
+                Create an Account
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   )
 }
 

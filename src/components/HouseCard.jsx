91b350f8dc8de36d783cb494033ce5b35937fc1f
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-03-03 18:13:01 +0700

    complete bookings component

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -7,19 +7,16 @@ import {
 import { comment } from 'postcss'
 import { Link } from 'react-router-dom'
 
-function HouseCard() {
+function HouseCard({ booking }) {
   return (
     <div className=" border rounded hover:shadow">
       <Link to="/houses/1">
-        <img
-          src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1640295026/portal/web%20development%20beginners/05%20Project%20Airbnb/house%2001/house_01_01.png
-"
-          className="border rounded"
-          alt=""
-        />
-        <h6 className="text-lg font-bold">Phuket, Thailand</h6>
-        <span className="text-sm text-slate-400">2 rooms · 2 bathrooms</span>
-        <h5 className="text-lg font-bold">$120</h5>
+        <img src={booking.location} className="border rounded" alt="" />
+        <h6 className="text-lg font-bold">{booking.location}</h6>
+        <span className="text-sm text-slate-400">
+          {booking.rooms} rooms · {booking.bathrooms} bathrooms
+        </span>
+        <h5 className="text-lg font-bold">${booking.price}</h5>
         <div className="flex justify-between">
           <div>
             <span>
@@ -28,10 +25,9 @@ function HouseCard() {
               <FontAwesomeIcon className="text-yellow-500" icon={faStar} />
               <FontAwesomeIcon className="text-yellow-500" icon={faStarHalf} />
             </span>
-            4.5
           </div>
           <div>
-            34{' '}
+            {booking.reviews}
             <FontAwesomeIcon
               className="text-transoarent"
               icon={faCommentDots}

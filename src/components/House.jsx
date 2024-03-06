5e3749a12f93239d9adf6b8d493707e4d40377f8
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-03-06 13:35:53 +0700

    Update House.jsx

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Nav from './Nav'
 import Gallery from './Gallery'
@@ -44,21 +44,24 @@ function BookHouse() {
 // Test in the browser that the House page displays the house from the API
 
 function House() {
-  const [house, setHouse] = useState({})
-  // let house = {
-  //   location: 'Phuket, Thailand',
-  //   rooms: 2,
-  //   bathrooms: 2,
-  //   description:
-  //     ' Nestled gracefully against the azure embrace of the ocean, the beautiful house exudes timeless charm and tranquility. Its weathered cedar exterior echoes the hues of the surrounding sand and sky, blending seamlessly with the coastal landscape. Large windows adorn its façade, inviting the golden sunlight to dance within its airy confines. A spacious deck overlooks the endless expanse of turquoise waters, offering panoramic views of rolling waves and distant horizons. Inside, the interiors are bathed in natural light, adorned with rustic furnishings and nautical accents that evoke a sense of seaside serenity. This coastal haven whispers tales of peace and rejuvenation.',
-  //   price: 300,
-  //   rating: 4,
-  //   host: {
-  //     firstName: 'Linda',
-  //     lastName: 'Smith',
-  //     picture: 'https://randomuser.me/api/portraits/women/85.jpg'
-  //   }
-  // }
+  const [house, setHouse] = useState(undefined)
+
+  const getHouse = async () => {
+    let { data } = await axios.get('https://haiku-bnb.onrender.com/houses/1')
+
+    console.log(data)
+    setHouse(data)
+
+    return house
+  }
+
+  useEffect(() => {
+    getHouse()
+  }, [])
+
+  if (house === undefined) {
+    return <div> 'loading'</div>
+  }
 
   return (
     <div className="container mx-auto">
@@ -67,29 +70,34 @@ function House() {
 
       <div className="grid grid-cols-3 gap-36 pb-10 mx-2">
         <div className="col-span-2">
-          {/* <div className=""> */}
-          <div className="text-lg font-bold my-4">{house.location}</div>
-          <div className="text-sm text-slate-400  my-4">
-            {house.rooms} rooms . {house.bathrooms} bathrooms
-          </div>
-          <div className="flex justify-start  my-4">
-            <img src={house.host.picture} className="rounded-full mr-2 h-12" />
-            <div>
-              <div className="text-sm">Hosted by</div>
-              <div className="text-sm">
-                <strong>
-                  {house.host.firstName} {house.host.lastName}
-                </strong>
+          <div className="">
+            <div className="text-lg font-bold my-4">{house.location}</div>
+            <div className="text-sm text-slate-400  my-4">
+              {house.rooms} rooms . {house.bathrooms} bathrooms
+            </div>
+            <div className="flex justify-start  my-4">
+              <img
+                src={house.host.picture}
+                className="rounded-full mr-2 h-12"
+              />
+              <div>
+                <div className="text-sm">Hosted by</div>
+                <div className="text-sm">
+                  <strong>
+                    {house.host.firstName} {house.host.lastName}
+                  </strong>
+                </div>
               </div>
             </div>
+            <div className="text-sm my-4">
+              {' '}
+              <p>{house.description}</p>{' '}
+            </div>
           </div>
-          <div className="text-sm my-4">
-            <p>{house.description}</p>
-          </div>
+          <BookHouse />
         </div>
-        <BookHouse />
+        <Reviews />
       </div>
-      <Reviews />
     </div>
   )
 }

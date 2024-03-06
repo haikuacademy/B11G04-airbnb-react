72c5871ad7b8bb8c1c6de5824d85c37a5f22c3aa
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-03-06 14:19:35 +0700

    Fixed redundant firstImage assignment

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 function Gallery({ house }) {
   let { images } = house
   const [selectedImage, setSelectedImage] = useState(images[0])
-  let firstImage = selectedImage
   return (
     <div className="grid grid-cols-2 gap-5 p-2">
-      <img src={firstImage} alt="House 1 image 1" className="rounded h-full" />
+      <img
+        src={selectedImage}
+        alt="House 1 image 1"
+        className="rounded h-full"
+      />
       <div className="grid grid-cols-3 gap-2">
         {images.map((image, id) => (
           <img

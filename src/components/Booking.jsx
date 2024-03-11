1fdff6c70c9ff34301216acfaf6bc92077d03c0d
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-03-11 12:59:04 +0700

    add createBooking function

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -5,6 +5,19 @@ function BookHouse({ house }) {
   const [endDate, setEndDate] = useState('')
   const [nights, setNights] = useState(0)
   const [totalPrice, setTotalPrice] = useState(0)
+
+  const createBooking = (e) => {
+    e.preventDefault()
+    // console.log(e.target)
+    const bookingContent = {
+      username: e.target.checkIn.value,
+      password: e.target.checkOut.value,
+      message: e.target.message.value
+    }
+
+    console.log(bookingContent)
+  }
+
   useEffect(() => {
     if (!startDate || !endDate) {
       return
@@ -25,7 +38,10 @@ function BookHouse({ house }) {
   }, [nights])
 
   return (
-    <form className="border-2 p-4 x-20 border-gray-300 rounded">
+    <form
+      onSubmit={(e) => createBooking(e)}
+      className="border-2 p-4 x-20 border-gray-300 rounded"
+    >
       <div className="pb-1 text-lg">
         <strong className="text-lg">$120</strong>/night
         <div className="flex">
@@ -34,6 +50,7 @@ function BookHouse({ house }) {
             <input
               className="border-2 p-2 my-2 mr-2 rounded"
               type="date"
+              name="checkIn"
               onChange={(e) => {
                 setStartDate(e.target.value)
               }}
@@ -44,6 +61,7 @@ function BookHouse({ house }) {
             <input
               className="border-2 p-2 my-2 rounded"
               type="date"
+              name="checkOut"
               onChange={(e) => {
                 setEndDate(e.target.value)
               }}
@@ -54,6 +72,7 @@ function BookHouse({ house }) {
           className="border-2 w-full p-2"
           cols="30"
           rows="6"
+          name="message"
           placeholder="Please send a message to the host..."
         ></textarea>
         <div className="flex justify-between">
